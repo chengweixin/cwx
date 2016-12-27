6bf59b8b9bbc5c024f8dc898db83125eb962f2a5
chengweixin9@126.com <chengweixin9@126.com>
2016-12-27 18:25:15 +0800

    第二次测试提交

--- MyBatis_Git/src/com/qingmai/HelloGit.java
+++ MyBatis_Git/src/com/qingmai/HelloGit.java
@@ -1,10 +1,13 @@
 package com.qingmai;
 
+import javax.print.StreamPrintService;
+
 public class HelloGit {
 
 	public static void main(String[] args) {
-		
-		
+
 		System.out.println("hello world");
+
+		System.out.println("ok??");
 	}
 }
